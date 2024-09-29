0e2a4a4700cccea9c2e9c14ab19fca400440d451
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-09-30 00:43:45 +0300

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation(libs.androidx.ui.tooling.preview.android)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
